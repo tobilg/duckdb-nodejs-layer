c7d74306b13ef2dfc161a4cbc734ffd36446aea8
tobilg <tobilg@gmail.com>
2023-02-11 18:25:26 +0100

    Fixes

--- publish_layer.sh
+++ publish_layer.sh
@@ -15,7 +15,7 @@ COMPATIBLE_ARCHITECTURES="x86_64"
 for region in ${REGIONS[@]}; do
   echo "Publishing layer to $region..."
 
-  LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description $DESCRIPTION --compatible-architectures $COMPATIBLE_ARCHITECTURES --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
+  LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-architectures "$COMPATIBLE_ARCHITECTURES" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
   POLICY=$(aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME --version-number $(echo -n $LAYER_ARN | tail -c 1) --statement-id $LAYER_NAME-public --action lambda:GetLayerVersion --principal \*)
   
   # echo $LAYER_ARN
