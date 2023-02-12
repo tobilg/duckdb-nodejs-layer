7f79dfb9fc92a1ea3a275c45611b555e7bdb9afa
tobilg <tobilg@gmail.com>
2023-02-12 11:26:55 +0100

    Activate multi-region publishing

--- publish_layer.sh
+++ publish_layer.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
 # AWS Regions
-REGIONS="us-east-1"
-# REGIONS=$(aws ssm get-parameters-by-path \
-#     --path /aws/service/global-infrastructure/regions \
-#     --query 'Parameters[].Value | sort(@)' 
-#     --output text)
+REGIONS=$(aws ssm get-parameters-by-path \
+    --path /aws/service/global-infrastructure/regions \
+    --query 'Parameters[].Value | sort(@)' 
+    --output text)
 
-LAYER_NAME="duckdb-node-layer-test"
+LAYER_NAME="duckdb-nodejs-layer"
 DESCRIPTION="Run DuckDB Node.js in AWS Lambda (https://github.com/tobilg/duckdb-lambda)"
 COMPATIBLE_RUNTIMES="nodejs14.x nodejs16.x nodejs18.x"
 COMPATIBLE_ARCHITECTURES="x86_64"
@@ -18,6 +17,6 @@ for region in ${REGIONS[@]}; do
   LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-architectures "$COMPATIBLE_ARCHITECTURES" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
   POLICY=$(aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME --version-number $(echo -n $LAYER_ARN | tail -c 1) --statement-id $LAYER_NAME-public --action lambda:GetLayerVersion --principal \*)
   
-  # echo $LAYER_ARN
+  echo $LAYER_ARN
   echo "$region complete"
 done
