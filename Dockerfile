1fa1f733238ea6437466d440548c4401e988d37e
Tobi <tobilg@gmail.com>
2023-02-27 13:11:20 +0000

    Updates

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,11 @@ RUN mkdir -p /tmp/from-git && cd /tmp/from-git && git clone https://github.com/d
 # Copy custom Makefile (less parallelism)
 COPY src/Makefile /tmp/from-git/duckdb/tools/nodejs/Makefile
 
+# Copy custom configure.py (add httpfs extension)
+COPY src/configure.py /tmp/from-git/duckdb/tools/nodejs/configure.py
+
 # Configure
-RUN cd /tmp/from-git/duckdb/tools/nodejs && ./configure && GEN=ninja EXTENSION_STATIC_BUILD=1 BUILD_TPCH=1 BUILD_HTTPFS=1 STATIC_OPENSSL=1 make
+RUN cd /tmp/from-git/duckdb/tools/nodejs && ./configure && GEN=ninja EXTENSION_STATIC_BUILD=1 BUILD_TPCH=1 BUILD_HTTPFS=1 STATIC_OPENSSL=1 BUILD_NODE=1 make
 
 # Copy local binding definition
 COPY src/lib/duckdb-binding.js /tmp/from-git/duckdb/tools/nodejs/lib/duckdb-binding.js
--- publish_layer.sh
+++ publish_layer.sh
@@ -11,11 +11,14 @@ COMPATIBLE_RUNTIMES="nodejs14.x nodejs16.x nodejs18.x"
 COMPATIBLE_ARCHITECTURES="x86_64"
 
 for region in ${REGIONS[@]}; do
-  echo "Publishing layer to $region..."
+  # Exclude gov and cn regions
+  if [[ ${region} != *"gov"* && ${region} != *"cn"* ]];then
+    echo "Publishing layer to $region..."
 
-  LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-architectures "$COMPATIBLE_ARCHITECTURES" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
-  POLICY=$(aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME --version-number $(echo -n $LAYER_ARN | tail -c 1) --statement-id $LAYER_NAME-public --action lambda:GetLayerVersion --principal \*)
-  
-  echo $LAYER_ARN
-  echo "$region complete"
+    LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-architectures "$COMPATIBLE_ARCHITECTURES" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
+    POLICY=$(aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME --version-number $(echo -n $LAYER_ARN | tail -c 1) --statement-id $LAYER_NAME-public --action lambda:GetLayerVersion --principal \*)
+    
+    echo $LAYER_ARN
+    echo "$region complete"
+  fi
 done
