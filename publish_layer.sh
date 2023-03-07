5d6ff14007d09237e0145140a79a3091b8e9b575
tobilg <tobilg@gmail.com>
2023-03-07 14:54:22 +0100

    Enable fts extension

--- publish_layer.sh
+++ publish_layer.sh
@@ -15,7 +15,7 @@ for region in ${REGIONS[@]}; do
   if [[ ${region} != *"gov"* && ${region} != *"cn"* ]];then
     echo "Publishing layer to $region..."
 
-    LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-architectures "$COMPATIBLE_ARCHITECTURES" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
+    LAYER_ARN=$(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME --description "$DESCRIPTION" --compatible-runtimes $COMPATIBLE_RUNTIMES --license MIT --zip-file fileb://release/duckdb-layer.zip | jq -r .LayerVersionArn)
     POLICY=$(aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME --version-number $(echo -n $LAYER_ARN | tail -c 1) --statement-id $LAYER_NAME-public --action lambda:GetLayerVersion --principal \*)
     
     echo $LAYER_ARN
