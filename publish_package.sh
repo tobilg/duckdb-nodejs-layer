2d9c70a4ac1bdf02a665c2a61a2093d4c9acbcc6
tobilg <tobilg@gmail.com>
2023-05-17 21:35:13 +0200

    Add publish_package.sh script

---
+++ publish_package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# Create layer folder
+mkdir -p $PWD/layer
+
+# Unzip to layer folder
+unzip -d layer/ $PWD/duckdb-layer.zip
+
+cd $PWD/layer/nodejs/node_modules/duckdb
+
+# Set package version to DuckDB version (make sure it doesn't fail if the same version exists locally)
+npm version $DUCKDB_VERSION 2>/dev/null || true
+
+# Check the lastest published package version
+CURRENT_PUBLISHED_PACKAGE_VERSION="${$(npm view duckdb-lambda version 2>/dev/null):-not_published}"
+# Check local package version
+CURRENT_LOCAL_PACKAGE_VERSION=$(node -p "require('./package.json').version")
+
+# Check if package shall be published
+if [[ "$CURRENT_PUBLISHED_PACKAGE_VERSION" = "not_published" || "$CURRENT_LOCAL_PACKAGE_VERSION" != "$CURRENT_PUBLISHED_PACKAGE_VERSION" ]]; then
+  # Publish to npm
+  npm publish
+fi
