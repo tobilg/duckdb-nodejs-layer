19295f355189207f3c0ce5f95f1f86da9fb3dcb8
Tobi <tobilg@gmail.com>
2023-09-20 16:52:52 +0000

    Intermediate version

--- src/duckdb_extension_config.cmake
+++ src/duckdb_extension_config.cmake
@@ -25,12 +25,3 @@ if (NOT WIN32)
             APPLY_PATCHES
             )
 endif()
-
-################# SPATIAL
-duckdb_extension_load(spatial
-        DONT_LINK LOAD_TESTS
-        GIT_URL https://github.com/duckdblabs/duckdb_spatial.git
-        GIT_TAG 44c9e49bdf48d39c936c500b632ad3bd74825b04
-        INCLUDE_DIR spatial/include
-        TEST_DIR test/sql
-        )
