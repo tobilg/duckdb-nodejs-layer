63ab05dcf754739a0125cb20231eb51dc1ff61b8
tobilg <tobilg@gmail.com>
2025-06-04 08:42:09 +0200

    Intermediate fixes

--- publish_package.sh
+++ publish_package.sh
@@ -9,7 +9,7 @@ unzip -d layer/ $PWD/duckdb-layer-x86_64.zip
 cd $PWD/layer/nodejs/node_modules/duckdb
 
 # Set package version to DuckDB version (make sure it doesn't fail if the same version exists locally)
-npm version $DUCKDB_VERSION-1 2>/dev/null || true
+npm version $DUCKDB_VERSION 2>/dev/null || true
 
 # Check the lastest published package version
 CURRENT_PUBLISHED_PACKAGE_VERSION="${$(npm view duckdb-lambda-x86 version 2>/dev/null):-not_published}"
